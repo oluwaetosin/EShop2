85e291b1efe0dbc1263a2b0c4afa1a07af48aeb8
Tosin Omotayo <oluwaetosin@gmail.com>
2025-05-18 16:35:12 +0100

    Add project files.

---
+++ Catalog.API/Program.cs
@@ -0,0 +1,16 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
